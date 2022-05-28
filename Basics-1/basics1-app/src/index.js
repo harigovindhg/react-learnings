18433f357d24836ecb6d945b4449414f3055c57c
harigovindhg <hgharigovind90@gmail.com>
2022-05-29 01:31:48 +0530

    created a basic react app and some basic knowledge of react components

--- Basics-1/basics1-app/src/index.js
+++ Basics-1/basics1-app/src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client"; // provides the render method
+// stateless functional component
+// always return JSX
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// function Greeting() {
+//   return <h1>Welcome to my first component</h1>;  // should always return something from functional components
+// }
+
+// ====================================================================================================
+
+// JSX rules:
+//  - return a single element
+//  - div / section / article or Fragment
+//  - use camelCase for html attributes
+//  - className instead of class
+//  - close every element
+//  - formatting
+
+// ====================================================================================================
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hello world')  //Using React.createElement to create JSX elements
+// }
+
+function Greeting() {
+  return (
+    // React fragment can be used to wrap a set of elements into a single element
+    <React.Fragment>
+      <div>
+        <GreetingHeading />
+        <ClickableLink />
+      </div>
+      <div></div>
+    </React.Fragment>
+  );
+}
+// Nesting Components in React
+const GreetingHeading = () => <h3>Hello People!</h3>;
+const ClickableLink = () => (
+  <ul>
+    <li>
+      <a href="#">Click this link!</a>
+    </li>
+  </ul>
 );
+// ====================================================================================================
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// IMPORTANT: As of March 2022, ReactDom is now deprecated, and is no longer ideal,
+// use ReactDom.createRoot instead
+
+// React 17 and below syntax
+// ReactDom.render(<Greeting />, document.getElementById("root")); // general syntax is render('what to render', 'where to render')
+
+// React 18 syntax
+const container = document.getElementById("root");
+const root = ReactDom.createRoot(container);
+root.render(<Greeting />);
